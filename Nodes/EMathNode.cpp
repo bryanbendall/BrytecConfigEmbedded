c4ab047816fa841ffaa03b2ec87991a6a7da775d
bryanbendall <b_bendall@hotmail.com>
2022-06-14 22:15:55 -0400

    work

--- Nodes/EMathNode.cpp
+++ Nodes/EMathNode.cpp
@@ -48,11 +48,50 @@ ENode* EMathNode::CreateInPlace(EMathNodeSpecification spec, uint8_t* destinatio
     return nullptr;
 }
 
+struct SerializedNodeInput {
+    uint8_t connectionNodeIndex;
+    uint8_t outputIndex;
+    float defaultValue;
+
+    ConnectionType getConnectionType()
+    {
+        if (connectionNodeIndex >= 0 && outputIndex >= 0)
+            return Pointer;
+        else
+            return Float;
+    }
+};
+
 ENode* EMathNode::DeserializeInPlace(BinaryDeserializer& des, uint8_t* destination)
 {
     EMathNodeSpecification spec;
-    spec.type = (MathType)des.readRaw<int>();
-    spec.input0 = (ConnectionType)des.readRaw<int>();
-    spec.input1 = (ConnectionType)des.readRaw<int>();
+
+    // Inputs
+    uint8_t inputCount = des.readRaw<uint8_t>();
+
+    // Input0
+    auto in = des.readRaw<SerializedNodeInput>();
+    spec.input0 = in.getConnectionType();
+
+    // Input1
+    auto in2 = des.readRaw<SerializedNodeInput>();
+    spec.input1 = in2.getConnectionType();
+
+    // // Input0
+    // if (des.readRaw<int8_t>() >= 0 && des.readRaw<int8_t>() >= 0)
+    //     spec.input0 = Pointer;
+    // des.readRaw<float>();
+
+    // // Input1
+    // if (des.readRaw<int8_t>() >= 0 && des.readRaw<int8_t>() >= 0)
+    //     spec.input0 = Pointer;
+    // des.readRaw<float>();
+
+    // Values
+    uint8_t valueCount = des.readRaw<uint8_t>();
+
+    // Math Type
+    spec.type = (MathType)des.readRaw<float>();
+
     return CreateInPlace(spec, destination);
 }
